fc8e8e9e2947d38e124264c28d071d53bff2ea22
Roos <nalloenoe.b@hotmail.com>
2020-12-11 14:48:00 +0100

    removed comments

--- BVH.cpp
+++ BVH.cpp
@@ -48,47 +48,6 @@ void BVH::FindSplitPlane()
 
 }
 
-/*
-/*class void BVH()
-{
-	Boundbox();
-	BVHnode* left=0, *right=0 //pointer to childs
-	vector<Primitive*> prims;
-	bool isLeaf = true;
-	void Intersect(Ray &r);
-	void Subdivide();
-	FindSplitPlane();
-}
-
-void BoundBox(MeshObject o) //one for mesh, one for circles, one for planes
-{
-	vec3 bmin, bmax; 
-	for()
-	{
-		if (p.x[i] < bmin.x) bmin.x = p.x[i];
-		if (p.y[i] < bmin.y) bmin.y = p.y[i];
-		if (p.z[i] < bmin.z) bmin.z = p.z[i];
-		if (p.x[i] > bmax.x) bmax.x = p.x[i];
-		if (p.y[i] > bmax.y) bmax.y = p.y[i];
-		if (p.z[i] > bmax.z) bmax.z = p.z[i];
-	}
-
-	return bmin bmax; 
-}
-
-
-void FindSplitPlane()
-{
-//pos and axis
-}
-
-void Intersect(Ray &r)
-{
-}
-
-void Subdivide()
-{
-}*/
 bool AABB::Contains( const point3 &p )
 {
 	return p.x > min.x && p.x < max.x &&
@@ -136,3 +95,4 @@ bool AABB::Intersect( const Ray &r )
 
 	return true;
 }
+
